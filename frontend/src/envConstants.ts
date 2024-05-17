9483062444a9aa643ea813e19137a91ec1bf5f46
yash <yp969803@gmail.com>
2024-05-17 22:43:04 +0530

    rfac: envConstant (frontend)

--- frontend/src/envConstants.ts
+++ frontend/src/envConstants.ts
@@ -1,4 +1,4 @@
-export const CLIENT_ID =process.env.CLIENT_ID;
-export const BACKEND_URL = process.env.BACKEND_URL;
-export const AVATAR_API = process.env.AVATAR_API;
+export const CLIENT_ID =process.env.REACT_APP_CLIENT_ID;
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const AVATAR_API = process.env.REACT_APP_AVATAR_API;
 
